517067b0c7297797cb463b44a3757cefafb6d984
André Sortland <arsortland@gmail.com>
2022-01-07 16:08:14 +0100

    structured even grid

--- script.js
+++ script.js
@@ -3,14 +3,19 @@
 function createGrid(){
     for (let i=0;i<16;i++){
         grids.appendChild(grid.cloneNode());
-        grid.textContent = "test";
-        console.log(grid.cloneNode());
+
     }
 }
 
+function hoverFunction(){
+    grid.classList.replace('grid', 'hovdiv');
+}
+
+
 const grid = document.createElement('div');
 grid.classList.add('grid');
-
 const grids = document.querySelector('.grids');
 
 createGrid();
+grid.addEventListener('click', hoverFunction);
+//Make it so that click-event triggers background color change!
