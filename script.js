f511c217b959fe72b483629722a51a8f437af51b
André Sortland <arsortland@gmail.com>
2022-01-09 14:07:49 +0100

    added eraser button and colorpicker is now working. Missing onclick typing and gradient colorchoice

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
-//Make new here? make a slide instead. Make sure all squares are actually square.
+
 function createGrid(n){
-    n = n*n;
-    for (let i=0;i<n;i++){
+    rowcol.style.gridTemplateColumns =`repeat(${n}, 1fr)`;
+    rowcol.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+    let gridsize = 100*100;
+    for (let i=0;i<gridsize;i++){
         grids.appendChild(grid.cloneNode());
     }
 }
 //can make an if "click" is true then do the function. In order to have a press and relase tpye of drawing.
 function hoverFunction(){
     selectAllGrids.forEach(item => {
-        item.addEventListener('mouseover', () =>{       //change to 'mouseover' later!
-            item.classList.replace('grid', 'hovdiv');
+        item.addEventListener('mouseover', () =>{
+            item.style.backgroundColor = colorpicker.value;  
         })
     });
 }
 
-
+//Selectors:
 const grid = document.createElement('div');
 grid.classList.add('grid');
 const grids = document.querySelector('.grids');
+const rowcol = document.getElementById("rowcol");
+
+//clear key:
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => location.reload());
+
+//Eraser:
+const eraser = document.querySelector('.eraser');
+eraser.addEventListener('click', () => colorpicker.value = '#FFFFFF');
+
+//Slider:
+const mySlider = document.getElementById('mySlider');
+const slidetext = document.querySelector('.slidetext');
+slidetext.textContent = mySlider.value +"x"+mySlider.value;
+mySlider.oninput = () => slidetext.textContent = mySlider.value+"x"+mySlider.value;
+
+//Colorpicker:
 const colorpicker = document.querySelector('#colorpicker');
 
-//Disse 3 linjene fungerer ikke! (eventuelt noe rekkefølge i tillegg)
-colorpicker.addEventListener('input', e => console.log(colorpicker.value));
-document.querySelectorAll('.hovdiv').forEach(item => {
-    item.style.backgroundColor = colorpicker.value;
-})
-
 //Programflow:
-createGrid(64);
+createGrid(mySlider.value);
+mySlider.addEventListener('mouseup', () => createGrid(mySlider.value));
 const selectAllGrids = document.querySelectorAll('.grid');
 hoverFunction();
 
 //TODO
-//Make 3 options: one with 500grids and min widt 5%.
-//  one with 2500grids and width 2%
-// one with 5000grids and width 1%
-
-//Make "clear key!"
-//also: COLORS! (make cool color selection wheel?)
 //also: ERASER (aka white color!)
