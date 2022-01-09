79423fda73750f653a31978eb4b800cf65ec88e3
André Sortland <arsortland@gmail.com>
2022-01-09 08:28:18 +0100

    the board is now working as intended

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-// make a function with one argument for number of grids?
-
-function createGrid(){
-    for (let i=0;i<16;i++){
+//Make new here? make a slide instead. Make sure all squares are actually square.
+function createGrid(n){
+    n = n*n;
+    for (let i=0;i<n;i++){
         grids.appendChild(grid.cloneNode());
-
     }
 }
-
+//can make an if "click" is true then do the function. In order to have a press and relase tpye of drawing.
 function hoverFunction(){
-    grid.classList.replace('grid', 'hovdiv');
+    selectAllGrids.forEach(item => {
+        item.addEventListener('mouseover', () =>{       //change to 'mouseover' later!
+            item.classList.replace('grid', 'hovdiv');
+        })
+    });
 }
 
 
 const grid = document.createElement('div');
 grid.classList.add('grid');
 const grids = document.querySelector('.grids');
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => location.reload());
+const colorpicker = document.querySelector('#colorpicker');
 
-createGrid();
-grid.addEventListener('click', hoverFunction);
-//Make it so that click-event triggers background color change!
+//Disse 3 linjene fungerer ikke! (eventuelt noe rekkefølge i tillegg)
+colorpicker.addEventListener('input', e => console.log(colorpicker.value));
+document.querySelectorAll('.hovdiv').forEach(item => {
+    item.style.backgroundColor = colorpicker.value;
+})
+
+//Programflow:
+createGrid(64);
+const selectAllGrids = document.querySelectorAll('.grid');
+hoverFunction();
+
+//TODO
+//Make 3 options: one with 500grids and min widt 5%.
+//  one with 2500grids and width 2%
+// one with 5000grids and width 1%
+
+//Make "clear key!"
+//also: COLORS! (make cool color selection wheel?)
+//also: ERASER (aka white color!)
